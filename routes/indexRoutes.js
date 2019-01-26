a1e3836ca9c6a31c086ab004fe470184701bd8c8
Jakecrpntr7 <carpejac@oregonstate.edu>
2019-01-26 11:46:22 -0500

    Seed data

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,7 +1,26 @@
+
+
 module.exports = (app) => {
 
   app.get('/', (req, res) => {
-    res.render('../views/home.handlebars', {css: ["main.css"]});
+    var context = {
+      css: [
+        "main.css",
+        "semantic.min.css",
+        "bootstrap.min.css"
+      ], 
+      js: [
+        "jquery-3.3.1.min.js",
+        "main.js",
+        "d3.v5.min.js",
+        "semantic.min.js",
+        "popper.min.js",
+        "bootstrap.min.js",
+        "seed.js"
+      ]
+    };
+
+    res.render('../views/home.handlebars', context);
   });
 
 }
