121dc1e434bbe1734571e55f94ca8b4b3b33f0c2
Jakecrpntr7 <carpejac@oregonstate.edu>
2019-01-31 18:57:03 -0500

    DepthCrawl

---
+++ public/js/crawl.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", function(e) {
+  $.ajax({
+    url: '/crawl/depth',
+    method: 'POST',
+    data: {steps: 5, word: 'This is a test word', url:'https://oregonstate.edu/'},
+    dataType: 'json',
+    success: function(res) {
+      populateTree(res);
+    },
+    error: function(err) {
+      alert('Error getting data');
+    },
+  });
+});
+
+function populateTree(tree) {
+  var treeCanvas = document.getElementById('tree-canvas');
+  var rootNode = tree[0];
+  var children = [];
+  rootNode.children.forEach(function(item) {
+    children.push(item);
+  });
+  //Children being changed here from list<integer> to list<object>
+  children = getChildrenByIdArray(children, tree);
+  treeCanvas.textContent = rootNode.url;
+  children.forEach(function (item) {
+    treeCanvas.textContent += '-> ' +  item.url;
+  });
+}
+
+function getChildrenByIdArray(children, tree) {
+  var returnChildren = [];
+  tree.forEach(function(item) {
+    children.forEach(function (childId) {
+      if(item.id == childId) {
+        returnChildren.push(item);
+      }
+    });
+  });
+  return returnChildren;
+}
