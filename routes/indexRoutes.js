cf48675e73ca80f5cf0d8a7a7465b4d361d7b71c
geographicdesign <joshua.l.hughes.pro@gmail.com>
2019-01-30 20:16:59 -0800

    updated nav bar

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -4,18 +4,19 @@ module.exports = (app) => {
 
   app.get('/', (req, res) => {
     var context = {
+      layout: "index",
       css: [
         "main.css",
-        "semantic.min.css",
-        "bootstrap.min.css"
+//        "semantic.min.css",
+//        "bootstrap.min.css"
       ], 
       js: [
         "jquery-3.3.1.min.js",
         "main.js",
         "d3.v5.min.js",
-        "semantic.min.js",
-        "popper.min.js",
-        "bootstrap.min.js",
+//        "semantic.min.js",
+//        "popper.min.js",
+//        "bootstrap.min.js",
         "seed.js"
 
       ]
