2d0a1408acf0f6d94f91c14c522ea9d587d540f2
geographicdesign <joshua.l.hughes.pro@gmail.com>
2019-03-10 22:57:45 -0700

    test btns off

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", main);
 var canvas;
 
 function main() {
-  d3.select("#test-data-btns").style("display", "block");
+//  d3.select("#test-data-btns").style("display", "block");
   window.addEventListener("hashchange", () => {
     if(window.location.hash == "#test") { 
       d3.select("#test-data-btns").style("display", "block");
